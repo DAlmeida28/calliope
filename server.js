dd4d9714d778e4225b373e4f61339a15730b50bd
Daniel Almeida <almeida.danny@gmail.com>
2025-02-25 11:29:08 -0600

    add login routes and response with token.

--- server.js
+++ server.js
@@ -2,18 +2,31 @@ require('dotenv').config();
 const client = require(`./db/client.js`);
 client.connect();
 
-const { registerUser } = require(`./db/users.js`);
+const { registerUser, loginUser } = require(`./db/users.js`);
 
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
+app.post(`/api/v1/login`, async (req,res, next) => {
+  try{
+    const { username, password } = req.body;
+
+    const token = await loginUser(username, password);
+    res.send({token: token});
+
+  } catch(err){
+    res.send({ message: `Bad Credentials.`}); 
+  }
+})
+
 app.post(`/api/v1/register`, async (req, res, next) => {
   try {
     const { username, password } = req.body;
     const userCreated = await registerUser(username, password);
     res.send(`${userCreated.username} created successfully.`);
+    
   } catch (err) {
     next(err);
   }
